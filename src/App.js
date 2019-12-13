f9c6c8ed4f54d1539ef2424eb23273b9272b3b2c
Naskiran <nasir.kiran@live.fr>
2019-12-13 14:20:44 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar/Navbar';
+import GlobalStyle from './styles/Global';
 
-function App() {
+class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen});
+  }
+
+  render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <Navbar
+       navbarState={this.state.navbarOpen}
+       handleNavbar={this.handleNavbar}
+       />
+       <GlobalStyle />
+   </>
   );
+}
+
 }
 
 export default App;
