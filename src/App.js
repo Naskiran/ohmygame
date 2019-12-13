c5cefd584ae9caee8027c32d01c0eb5caf151696
Naskiran <nasir.kiran@live.fr>
2019-12-13 20:56:47 +0100

    footer on progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from './components/navbar/Navbar';
 import GlobalStyle from './styles/Global';
 import './App.css'
+import Footer from 'rc-footer';
+import 'rc-footer/assets/index.css';
 
 class App extends Component {
   state = {
@@ -21,6 +23,16 @@ class App extends Component {
        handleNavbar={this.handleNavbar}
        />
        <GlobalStyle />
+       <Footer
+         columns={[{
+         icon: (
+         <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />),
+         url: 'https://github.com/Naskiran',
+         openExternal: true,
+      },
+    ]}
+    bottom="Made with ❤️ by Naskiran"
+      />
    </>
   );
 }
