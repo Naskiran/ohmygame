fe102c223a9b28dfe9828976bf3f7afa4a5ba950
Naskiran <nasir.kiran@live.fr>
2019-12-17 23:29:49 +0100

    adminform onprogress

---
+++ src/components/adminform.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+const SimpleForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>GAME</label>
+        <div>
+          <Field
+            name="Game"
+            component="input"
+            type="text"
+            placeholder="Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Price</label>
+        <div>
+          <Field
+            name="Price"
+            component="input"
+            type="text"
+            placeholder="Price"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default reduxForm({
+  form: 'simple', // a unique identifier for this form
+})(SimpleForm);
