7dad1de620cd955afa33639245e6bdcaa4d52160
Naskiran <nasir.kiran@live.fr>
2019-12-17 17:23:50 +0100

    server file

---
+++ src/conf.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+require('dotenv').config(process.cwd(), '.env')
+
+const connection = mysql.createConnection({
+    multipleStatements: true,
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_DB,
+});
+
+module.exports = connection;
